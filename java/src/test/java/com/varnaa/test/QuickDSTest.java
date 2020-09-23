b81b7aaea6bf0c6de4700def1206238fcb9fe52e
swethavarnaa <swethavarnaa@gmail.com>
2020-09-23 23:10:33 +0530

    Added unit testing for BST

--- java/src/test/java/com/varnaa/test/QuickDSTest.java
+++ java/src/test/java/com/varnaa/test/QuickDSTest.java
@@ -2,6 +2,7 @@ package com.varnaa.test;
 
 import com.varnaa.quickds.Node;
 import com.varnaa.quickds.Quickds;
+import com.varnaa.quickds.TreeNode;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -72,8 +73,34 @@ public class QuickDSTest {
     @Test
     public void createRandomLinkedListTest() {
         Node node = quickds.createRandomLinkedList();
-        Assertions.assertTrue(node != null);
+        assertNotNull(node);
     }
 
+    @Test
+    public void createBST() {
+        // Random
+        TreeNode root = quickds.createRandomBST();
+        assertNotNull(root);
+
+
+        // Custom
+        TreeNode customRoot = quickds.createCustomBST(45);
+
+
+        quickds.prettyPrintBST(root);
+        quickds.prettyPrintBST(customRoot);
+        quickds.printBST(root);
+        quickds.prettyPrintBST(customRoot);
+    }
+
+
+    @Test
+    public void formatingBSTTest() {
+        // String input
+        String input = "[41,37,44,24,39,42,48,1,35,38,40,null,43,46,49,0,2,30,36,null,null,null,null,null,null,45,47,null,null,null,null,null,4,29,32,null,null,null,null,null,null,3,9,26,null,31,34,null,null,7,11,25,27,null,null,33,null,6,8,10,16,null,null,null,28,null,null,5,null,null,null,null,null,15,19,null,null,null,null,12,null,18,20,null,13,17,null,null,22,null,14,null,null,21,23]";
+        TreeNode root = quickds.formatStringToBST(input);
+        quickds.printBST(root);
+        quickds.prettyPrintBST(root);
+    }
 
 }
